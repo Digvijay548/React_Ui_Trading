49f54eaf97af23b79e841cd15fc286bac0dc0836
Digvijay Patil <160723513+Digvijay548@users.noreply.github.com>
2025-02-25 21:43:11 +0530

    session timeout

--- online/src/index.js
+++ online/src/index.js
@@ -13,11 +13,11 @@ import TermsAndConditions from "./pages/TermsAndConditions";
 import { AuthProvider, useAuth } from "./pages/authContext";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState } from "react";
+import { useState,useEffect  } from "react";
 import { FaRobot, FaChartLine, FaSignOutAlt, FaSignInAlt, FaGift, FaWallet, FaEnvelope, FaFileContract, FaEllipsisV } from "react-icons/fa";
 import { motion } from "framer-motion";
 
-const App = () => {
+const App = () => { 
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   const toggleNav = () => setIsNavOpen(!isNavOpen);
@@ -61,24 +61,28 @@ const App = () => {
     <AuthProvider>
       <Router>
         <div>
-          <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm px-3">
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm ">
             <div className="container-fluid">
               <Link className="navbar-brand d-flex align-items-center" to="/home">
                 <motion.div animate={{ rotate: [0, 10, -10, 0], scale: [1, 1.1, 1] }} transition={{ repeat: Infinity, duration: 2 }}>
                   <FaRobot className="text-primary" size={45} />
                 </motion.div>
-                <h2 className="fw-bold text-uppercase px-2 site-title">
-                  <FaChartLine className="text-warning" size={28} /> AI Trading Dashboard 📊🚀
-                </h2>
+                <h4 className="fw-bold text-uppercase px-2 site-title">
+                  <FaChartLine className="text-warning" size={45} /> AI Trading Dashboard 📊🚀
+                </h4>
               </Link>
               <button className="navbar-toggler" type="button" onClick={toggleNav}>
                 <span className="navbar-toggler-icon"></span>
               </button>
               <div className={`collapse navbar-collapse ${isNavOpen ? "show" : ""}`} id="navbarNav">
                 <ul className="navbar-nav ms-auto text-center">
-                  <li className="nav-item">
-                    <Link to="/home" className="nav-link text-white fw-bold" onClick={closeNav}>Home</Link>
-                  </li>
+                  
+                    <li className="nav-item">
+                      <Link to="/home" className="nav-link text-white fw-bold" onClick={closeNav}>
+                        Home
+                      </Link>
+                    </li>
+                  
                   <li className="nav-item">
                     <Link to="/about" className="nav-link text-white fw-bold" onClick={closeNav}>About</Link>
                   </li>
